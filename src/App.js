bcf066a8e3405b8ef505e669e7f4c0a1ecf7c4ea
yogesh upadhyay <yogeshh2021@gmail.com>
2021-06-18 23:09:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Navbar } from 'argon-design-system-free';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 class="display-1">Display 1</h1>
     </div>
   );
 }
